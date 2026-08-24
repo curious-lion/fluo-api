62731d9f4f031da6cf8ba549af420cd238d733da
diogoabreu <diogoalmeidaabreu@gmail.com>
2026-08-24 18:14:56 +0100

    style: add proprietary copyright headers to Java source files

--- src/main/java/com/fluo/api/FluoApiApplication.java
+++ src/main/java/com/fluo/api/FluoApiApplication.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2026 Fluo API. All rights reserved.
+ * This software is proprietary. Unauthorized copying, redistribution,
+ * or commercial use is strictly prohibited under the terms defined
+ * in the project's LICENSE and README.md.
+ */
 package com.fluo.api;
 
 import org.springframework.boot.SpringApplication;
